7ce6c2da00e5a39ad8d8773fb9d2e7a4a7b9e194
annikaputt <annika@instream.net>
2015-08-21 11:42:23 -0700

    Created a new file to upload data

---
+++ BRC_DailyUpload.R
@@ -0,0 +1,10 @@
+############################
+# BRC_DataUpload.R
+# BRC_DailyReportVisuals.Rproj
+
+# Uploads historic data obtained by Dorian Turner, then uploads any files 
+# that contain the text "BRC Daiy Report Mail"
+
+# Created August 21, 2015
+# A Putt
+#############################
