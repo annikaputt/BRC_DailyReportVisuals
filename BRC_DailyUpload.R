da9b49931a74d761d8cfda882cea8af4b3804062
annikaputt <annika@instream.net>
2015-08-25 13:48:28 -0700

    August 25, 2015 edits

--- BRC_DailyUpload.R
+++ BRC_DailyUpload.R
@@ -15,7 +15,7 @@ dr <- read.csv("Down_2010_Nov52014.csv",head=TRUE)
 
 # Create a function to average daily levels
 DailyLevels <- function(data) {
-  data$Date <- as.Date(data$Date,"%Y-%m-%")
+  data$Date <- as.Date(data$Date,format="%Y-%m-%d")
   daily <- data.frame(do.call("rbind",as.list(by(data[,"Value"],data$Date,mean))))
   daily$Date <- row.names(daily)
   colnames(daily) <- c("Levels","Date")
@@ -30,12 +30,11 @@ drdaily <- DailyLevels(dr)
 ##########
 # Upload all other BRC files. These are downloaded from the email and saved as .csv files in 
 # the working directory.
-file_list  <- list.files(path="DailyReportMails", pattern="*.csv")
-names <- paste(substr(file_list,1,3),substr(file_list,23,26),sep="")
+file_list  <- list.files(path="DailyReportMails", pattern="*.csv") # Pull all files
+names <- paste(substr(file_list,1,3),substr(file_list,23,26),sep="") # Pull the file names
 
 for(i in 1:length(file_list)){
-  assign(names[i], read.csv(paste("DailyReportMails/",file_list[i],sep=""),
-                            skip=9))
+  assign(names[i], read.csv(paste("DailyReportMails/",file_list[i],sep=""),skip=9)) # Read in the data
 }
 df_list = lapply(ls(pattern = "BRC"), get) # Turn into a list
 names(df_list) <- ls(pattern = "BRC")
@@ -45,13 +44,13 @@ newList <- list()
 for(i in 1:length(df_list) ){
   temp <- df_list[[i]]
   removeUnwanted <- temp[1:31,1:11]
-  colnames(removeUnwanted) <- c("day","downtonelev","carpenterelev","setondamelev","terzaghiflow","setonsyphons","setonriverflow","setonrivercfs",
+  colnames(removeUnwanted) <- c("day","downtonelev","carpenterelev","terzaghiflow","setondamelev","setonsyphons","setonriverflow","setonrivercfs",
                                 "cayooshflow","dilutionpercent","fraserhopeflow")
   removeUnwanted$year <- paste("20",substr(names(df_list[i]),4,5), sep="")
   removeUnwanted$month <- substr(names(df_list[i]),6,7)
   removeNA <- removeUnwanted[complete.cases(removeUnwanted[4]),] # remove any data where the second column has an na (gets rid of day 31 for months with 30 days)
-  removeNA$downtonelev <- as.numeric(removeNA$downtonelev) # Because there were some phone numbers in the file these were factors
-  removeNA$carpenterelev <- as.numeric(removeNA$downtonelev)
+  removeNA$downtonelev <- as.numeric(as.character(removeNA$downtonelev)) # Because there were some phone numbers in the file these were factors. Need to convert them to characters and then numbers
+  removeNA$carpenterelev <- as.numeric(as.character(removeNA$carpenterelev))
   newList[[i]] <- removeNA
 }
 
@@ -72,3 +71,8 @@ Historic2 <- data.frame(day=Historic$day,downtonelev=Historic$downtonelev,carpen
 allData <- rbind(DailyMail,Historic2)
 allData$fulldate <- paste( allData$year, allData$month, allData$day, sep = "/")
 allData$fulldate <- as.POSIXct(allData$fulldate)
+
+# Add some more date columns to the main data frame
+allData$shortdate <- format(allData$fulldate,"%b-%d")
+allData$fulldate <- as.POSIXlt(allData$fulldate)
+allData$daynumber <- allData$fulldate$yday # January 1 is 0
---
+++ BRC_Plots.R
@@ -0,0 +1,93 @@
+############################
+# BRC_Plots.R
+# BRC_DailyReportVisuals.Rproj
+
+# Generates plots of data from BRC daily data
+
+# Created August 24, 2015
+# A Putt
+#############################
+
+library(lattice)
+
+source("BRC_DailyUpload.R") #allData contains the uploaded data
+
+head(allData)
+
+#############
+# Create a dummy vector for x axis labels
+temp <- subset(allData,year=="2010")
+temp <- subset(temp,shortdate %in% c("Jan-01","Feb-01","Mar-01","Apr-01","May-01","Jun-01","Jul-01","Aug-01","Sep-01","Oct-01","Nov-01","Dec-01"))
+at <- temp$daynumber
+labels <- temp$shortdate
+
+############
+# CARPENTER
+# If you want to add anything to the plot, such as the date of ko arrival, it is convenient to be able to plot the day number
+# You will need to pull the dates manually and find the day number so that they can be plotted for any year; i.e., the axis is general to all years
+# Create day number vectors for dates of ko in streamwalks and bt up streams in 2014
+vertical <- subset(allData,shortdate %in% c("Aug-19","Sep-03","Sep-16","Oct-01") & year=="2010")$daynumber
+
+levelsplot.carpenter <- 
+  xyplot(allData$carpenterelev~allData$daynumber,groups=as.factor(allData$year),type="l",
+         xlab=list(label=NULL),
+         ylab=list(label="Reservoir Elevation (m)"),
+         main="Carpenter Reservoir Elevation",
+         col=1:6,lwd=2,
+         scales=list(tck=c(1,0),alternating=1,x=list(at=at,labels=labels,axs="i")),
+         key=list(corner=c(1,0.05),
+                  lines=list(col=c(1:6,"black","red"),lwd=2,lty=c(1,1,1,1,1,1,2,2)),
+                  text=list(labels=c(levels(as.factor(allData$year)),"2014 KO Sightings","2014 BT Tracked in Tribs"))),
+         panel=function(...) {
+           panel.rect(xleft=c(vertical[1]), xright=c(vertical[4]),ybottom=0, ytop=700,col=c("lightgrey"),border=NA)
+           panel.abline(v=vertical,lty=2,lwd=2,col=c("black","red","black","red"))
+           panel.xyplot(...)
+         })
+
+windows()
+print(levelsplot.carpenter)
+
+png("CarpReservoirElevations.png",width=11,height=8.5,units="in",res=77)
+levelsplot.carpenter
+dev.off()
+
+##########
+# DOWNTON
+
+levelsplot.downton <- 
+  xyplot(allData$downtonelev~allData$daynumber,groups=as.factor(allData$year),type="l",
+         xlab=list(label=NULL),
+         ylab=list(label="Reservoir Elevation (m)"),
+         main="Downton Reservoir Elevation",
+         col=1:6,lwd=2,
+         scales=list(tck=c(1,0),alternating=1,x=list(at=at,labels=labels,axs="i")),
+         key=list(corner=c(1,0.05),lines=list(col=c(1:6),lwd=2,lty=c(1,1,1,1,1,1)),
+             text=list(labels=c(levels(as.factor(allData$year)))))
+)
+
+windows()
+print(levelsplot.downton)
+
+png("DowntonReservoirElevations.png",width=11,height=8.5,units="in",res=77)
+levelsplot.downton
+dev.off()
+
+##########
+# MIDDLE BRIDGE RIVER
+
+tzflowplot <- 
+  xyplot(allData$terzaghiflow~allData$daynumber,groups=as.factor(allData$year),type="l",
+         xlab=list(label=NULL),
+         ylab=list(label="Terzaghi Flow (m3/s)"),
+         main="Terzaghi Flow",lwd=2,col=c(1),
+         scales=list(tck=c(1,0),alternating=1,x=list(at=at,labels=labels,axs="i")),
+         key=list(corner=c(1,0.05),lines=list(col=c(1),lwd=2),
+                  text=list(labels=c("2015")))
+  )
+
+windows()
+print(tzflowplot)
+
+png("TerzaghiFlow.png",width=11,height=8.5,units="in",res=77)
+tzflowplot
+dev.off()
